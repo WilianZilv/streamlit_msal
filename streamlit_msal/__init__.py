1804923845872af31e97a01c37226dce4aaa709a
Wilian <wilianzilv@gmail.com>
2023-11-18 13:37:24 -0300

    Complete refactor of the lib

--- streamlit_msal/__init__.py
+++ streamlit_msal/__init__.py
@@ -1,10 +1,10 @@
-from dataclasses import dataclass
 import os
 from typing import Literal, Optional
 import streamlit.components.v1 as components
 import streamlit as st
+from streamlit import session_state as ss
 
-_RELEASE = True
+_RELEASE = False
 
 
 def declare_component(name: str):
@@ -24,28 +24,100 @@ def declare_component(name: str):
     return components.declare_component(name, path=build_dir)
 
 
-def msal(
-    trigger_login: bool,
-    trigger_logout: bool,
-    client_id: str,
-    authority: str,
-    scopes=[],
-    key=None,
-):
-    key = key or "streamlit_msal.msal"
+action_key = "streamlit_msal.msal:action"
 
-    _component = declare_component("msal")
 
-    value = _component(
-        login=trigger_login,
-        logout=trigger_logout,
-        clientId=client_id,
-        authority=authority,
-        scopes=scopes,
-        key=key,
-    )
+class Msal:
+    @staticmethod
+    def initialize(
+        client_id: str,
+        authority: str,
+        scopes=[],
+        key=None,
+    ):
+        key = key or "streamlit_msal.msal"
 
-    if value is None:
-        st.stop()
+        action: Optional[Literal["login", "logout"]] = ss.get(action_key, None)
 
-    return value["data"]
+        trigger_login = action == "login"
+        trigger_logout = action == "logout"
+        revalidate = action == "revalidate"
+
+        _component = declare_component("msal")
+
+        value = _component(
+            login=trigger_login,
+            logout=trigger_logout,
+            revalidate=revalidate,
+            clientId=client_id,
+            authority=authority,
+            scopes=scopes,
+            key=key,
+        )
+
+        if (value or {}).get("in_interaction", False):
+            st.stop()
+
+        if value is None:
+            st.stop()
+
+        ss[action_key] = None
+
+        return value["data"]
+
+    @staticmethod
+    def sign_in():
+        ss[action_key] = "login"
+        st.rerun()
+
+    @staticmethod
+    def sign_out():
+        ss[action_key] = "logout"
+        st.rerun()
+
+    @staticmethod
+    def revalidate():
+        ss[action_key] = "revalidate"
+        st.rerun()
+
+    @staticmethod
+    def initialize_ui(
+        client_id,
+        authority,
+        scopes=[],
+        connecting_label="Connecting",
+        disconnected_label="Disconnected",
+        sign_in_label="Sign in",
+        sign_out_label="Sign out",
+    ):
+        with st.status(connecting_label) as status:
+            auth_data = Msal.initialize(
+                client_id=client_id, authority=authority, scopes=scopes
+            )
+
+            is_authenticated = auth_data is not None
+
+            if is_authenticated:
+                name = auth_data["account"]["name"]
+                status.update(label=name)
+            else:
+                status.update(label=disconnected_label, expanded=True, state="error")
+
+            action = "login" if not is_authenticated else "logout"
+
+            label_map = {
+                "login": sign_in_label,
+                "logout": sign_out_label,
+            }
+
+            do_action = st.button(label_map[action], use_container_width=True)
+
+            action = action if do_action else None
+
+            if action == "login":
+                Msal.sign_in()
+
+            if action == "logout":
+                Msal.sign_out()
+
+        return auth_data
